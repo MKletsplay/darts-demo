f6c03105550f20481a1948b89713824f797d660f
MKletsplay <118121481+MKletsplay@users.noreply.github.com>
2023-01-22 11:59:06 +0100

    Add files via upload

---
+++ Darts/script.js
@@ -0,0 +1,353 @@
+
+
+        
+ var currentInputId = "demo1"
+
+
+//Eingaben zurücksetzen
+resetValue()
+function resetValue() {
+
+    document.getElementById("demo1").value = "/" ;
+    document.getElementById("demo2").value = "/" ;
+    document.getElementById("demo3").value = "/" ;
+
+}
+
+    
+//Wurf daneben (0 Punkte)
+myFunction0()
+function myFunction0() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 0 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+
+    document.getElementById("demo1").onclick =  myFunction0;
+    document.getElementById("demo2").onclick =  myFunction0;
+    document.getElementById("demo3").onclick =  myFunction0;
+}
+
+
+        
+
+ // 20er Reihe
+myfunctionD20()
+function myfunctionD20() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 40 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+            
+    document.getElementById("demo1").onclick =  myfunctionD20;
+    document.getElementById("demo2").onclick =  myfunctionD20;
+    document.getElementById("demo3").onclick =  myfunctionD20;
+}
+
+         
+myfunction20()
+function myfunction20() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 20 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+
+    document.getElementById("demo1").onclick =  myfunction20;
+    document.getElementById("demo2").onclick =  myfunction20;
+    document.getElementById("demo3").onclick =  myfunction20;
+}
+
+
+myfunctionT20()
+function myfunctionT20() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 60 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+            
+    document.getElementById("demo1").onclick =  myfunctionT20;
+    document.getElementById("demo2").onclick =  myfunctionT20;
+    document.getElementById("demo3").onclick =  myfunctionT20;
+}
+
+myFunction20()
+function myFunction20() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 20 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+            
+    document.getElementById("demo1").onclick =  myFunction20;
+    document.getElementById("demo2").onclick =  myFunction20;
+    document.getElementById("demo3").onclick =  myfunction20;
+}
+
+
+
+
+// 1er Reihe
+myfunctionD1()
+function myfunctionD1() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 2 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+            
+    document.getElementById("demo1").onclick =  myfunctionD1;
+    document.getElementById("demo2").onclick =  myfunctionD1;
+    document.getElementById("demo3").onclick =  myfunctionD1;
+}
+
+         
+myfunction1()
+function myfunction1() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 1 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+
+    document.getElementById("demo1").onclick =  myfunction1;
+    document.getElementById("demo2").onclick =  myfunction1;
+    document.getElementById("demo3").onclick =  myfunction1;
+}
+
+
+myfunctionT1()
+function myfunctionT1() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 3 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+            
+    document.getElementById("demo1").onclick =  myfunctionT1;
+    document.getElementById("demo2").onclick =  myfunctionT1;
+    document.getElementById("demo3").onclick =  myfunctionT1;
+}
+
+myFunction1()
+function myFunction1() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 1 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+            
+    document.getElementById("demo1").onclick =  myFunction1;
+    document.getElementById("demo2").onclick =  myFunction1;
+    document.getElementById("demo3").onclick =  myFunction1;
+}
+
+
+
+
+// 18er Reihe
+myfunctionD18()
+function myfunctionD18() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 36 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+            
+    document.getElementById("demo1").onclick =  myfunctionD18;
+    document.getElementById("demo2").onclick =  myfunctionD18;
+    document.getElementById("demo3").onclick =  myfunctionD18;
+}
+
+         
+myfunction18()
+ function myfunction18() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 18 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+
+    ocument.getElementById("demo1").onclick =  myfunction18;
+    document.getElementById("demo2").onclick =  myfunction18;
+    document.getElementById("demo3").onclick =  myfunction18;
+}
+
+
+myfunctionT18()
+function myfunctionT18() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 54 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+            
+    document.getElementById("demo1").onclick =  myfunctionT18;
+    document.getElementById("demo2").onclick =  myfunctionT18;
+    document.getElementById("demo3").onclick =  myfunctionT18;
+}
+
+myFunction18()
+function myFunction18() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 18 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+            
+    document.getElementById("demo1").onclick =  myFunction18;
+    document.getElementById("demo2").onclick =  myFunction18;
+    document.getElementById("demo3").onclick =  myFunction18;
+}
+
+
+
+
+//4er Reihe 
+myfunctionD4()
+function myfunctionD4() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 8 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+            
+    document.getElementById("demo1").onclick =  myfunctionD4;
+    document.getElementById("demo2").onclick =  myfunctionD4;
+    document.getElementById("demo3").onclick =  myfunctionD4;
+}
+
+         
+myfunction4()
+ function myfunction4() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 4 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+
+    document.getElementById("demo1").onclick =  myfunction4;
+    document.getElementById("demo2").onclick =  myfunction4;
+    document.getElementById("demo3").onclick =  myfunction4;
+}
+
+
+myfunctionT4()
+function myfunctionT4() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 12 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+            
+    document.getElementById("demo1").onclick =  myfunctionT4;
+    document.getElementById("demo2").onclick =  myfunctionT4;
+    document.getElementById("demo3").onclick =  myfunctionT4;
+}
+
+myFunction4()
+function myFunction4() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 4 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+            
+    document.getElementById("demo1").onclick =  myFunction4;
+    document.getElementById("demo2").onclick =  myFunction4;
+    document.getElementById("demo3").onclick =  myFunction4;
+}
+
+
+
+
+//13er Reihe
+myfunctionD13()
+function myfunctionD13() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 26 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+            
+    document.getElementById("demo1").onclick =  myfunctionD13;
+    document.getElementById("demo2").onclick =  myfunctionD13;
+    document.getElementById("demo3").onclick =  myfunctionD13;
+}
+
+         
+myfunction13()
+ function myfunction13() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 13 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+
+    document.getElementById("demo1").onclick =  myfunction13;
+    document.getElementById("demo2").onclick =  myfunction13;
+    document.getElementById("demo3").onclick =  myfunction13;
+}
+
+
+myfunctionT13()
+function myfunctionT13() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 39 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+            
+    document.getElementById("demo1").onclick =  myfunctionT13;
+    document.getElementById("demo2").onclick =  myfunctionT13;
+    document.getElementById("demo3").onclick =  myfunctionT13;
+}
+
+myFunction13()
+function myFunction13() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 13 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+            
+    document.getElementById("demo1").onclick =  myFunction13;
+    document.getElementById("demo2").onclick =  myFunction13;
+    document.getElementById("demo3").onclick =  myFunction13;
+}
+
+
+
+
+//6er Reihe
+myfunctionD6()
+function myfunctionD6() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 12 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+            
+    document.getElementById("demo1").onclick =  myfunctionD6;
+    document.getElementById("demo2").onclick =  myfunctionD6;
+    document.getElementById("demo3").onclick =  myfunctionD6;
+}
+
+         
+myfunction6()
+ function myfunction6() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 6 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+
+    document.getElementById("demo1").onclick =  myfunction6;
+    document.getElementById("demo2").onclick =  myfunction6;
+    document.getElementById("demo3").onclick =  myfunction6;
+}
+
+
+myfunctionT6()
+function myfunctionT6() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 18 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+            
+    document.getElementById("demo1").onclick =  myfunctionT6;
+    document.getElementById("demo2").onclick =  myfunctionT6;
+    document.getElementById("demo3").onclick =  myfunctionT6;
+}
+
+myFunction6()
+function myFunction6() {
+            
+    var currentInput = document.getElementById(currentInputId);
+    currentInput.value = 6 ;
+    currentInputId = (currentInputId == "demo1") ? "demo2" : (currentInputId == "demo2") ? "demo3" : "demo1";
+            
+    document.getElementById("demo1").onclick =  myFunction6;
+    document.getElementById("demo2").onclick =  myFunction6;
+    document.getElementById("demo3").onclick =  myFunction6;
+}
